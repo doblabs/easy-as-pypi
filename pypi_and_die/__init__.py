7e3a5f86407d1cfda342f524cf2bc4b4f9ec8449
landonb <landonb@users.noreply.github.com>
2020-03-30 16:33:01 -0500

    Docs: Update copyright.

--- pypi_and_die/__init__.py
+++ pypi_and_die/__init__.py
@@ -33,15 +33,15 @@ from pypi_and_die import commands
 
 __all__ = (
     '__arg0name__',
-    '__author__',
-    '__author_email__',
+    '__author_name__',
+    '__author_link__',
     '__package_name__',
 )
 
 # NOT_DRY: (lb): These strings also found in setup.cfg, but I'm not sure how best
 # to DRY. Fortunately, they're not likely to change. Useful for UX copyright text.
-__author__ = 'HotOffThe Hamster'
-__author_email__ = 'hotoffthehamster+releaseghubpypi@gmail.com'
+__author_name__ = 'Landon Bouma'
+__author_link__ = 'https://tallybark.com'
 
 # (lb): Not sure if the package name is available at runtime. Seems kinda meta,
 # like, Who am I? Useful for calling get_distribution, or to avoid hardcoding
