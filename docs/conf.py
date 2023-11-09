bd16ad04b44eb96af842cea0c4c7fb3c07b434b5
Landon Bouma <landonb@users.noreply.github.com>
2023-11-09 05:03:17 -0600

    Docs: Cleanup comments

--- docs/conf.py
+++ docs/conf.py
@@ -20,9 +20,7 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-# import datetime
 import os
-# import shlex
 import sys
 from collections.abc import MutableMapping
 
